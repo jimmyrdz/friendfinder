cee3bacd6e742f2a6de9f782ca80b4e704252aa9
jimmyrdz <ardzjaime@gmail.com>
2019-07-09 00:20:56 -0500

    added api routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
